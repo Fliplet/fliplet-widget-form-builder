6efa998df2a8692e5e5cce3e432c16316fa4ec78
miqur <treshom@gmail.com>
2019-08-06 00:02:35 +0300

    update-form-builder-fields-4768

--- js/libs/core.js
+++ js/libs/core.js
@@ -159,11 +159,7 @@ Fliplet.FormBuilder = (function() {
 
       // On submit event
       component.methods._onSubmit = function() {
-        if (this.name === '') {
-          this.name = this.label;
-        }
-        
-        if (this._fieldNameError) {
+        if (this._fieldNameError || this._fieldLabelError) {
           return;
         }
 
@@ -181,13 +177,6 @@ Fliplet.FormBuilder = (function() {
 
       if (!component.methods.onSubmit) {
         component.methods.onSubmit = component.methods._onSubmit;
-      }
-
-      if (!component.mounted) {
-        component.mounted = function() {
-          this._showNameField = this.name !== this.label;
-          this.initTooltip();
-        };
       }
 
       component.props._fields = {
@@ -227,7 +216,7 @@ Fliplet.FormBuilder = (function() {
   
       component.computed._fieldLabelError = function() {
         if (!this.label) {
-          return 'Please provide a Field name & label';
+          return 'Please provide a Field Label';
         }
     
         var existing = _.findIndex(this._fields, {
@@ -235,7 +224,7 @@ Fliplet.FormBuilder = (function() {
         });
     
         if (existing > -1 && existing !== this._idx) {
-          return this.name + ' is taken. Please use another Field Name.';
+          return this.name + ' is taken. Please use another Field Label.';
         }
     
         return '';
@@ -243,12 +232,20 @@ Fliplet.FormBuilder = (function() {
   
       component.methods._addCustomName = function() {
         this._showNameField = !this._showNameField;
+        this.initTooltip();
+      };
+
+      if (!component.methods.addCustomName) {
+        component.methods.addCustomName = component.methods._addCustomName;
+      }
+
+      component.methods._compareFieldName = function() {
         this.name = this.label;
         this.initTooltip();
       };
-  
-      if (!component.methods.addCustomName) {
-        component.methods.addCustomName = component.methods._addCustomName;
+
+      if (!component.methods.compareFieldName) {
+        component.methods.compareFieldName = component.methods._compareFieldName;
       }
   
       component.methods._initTooltip = function() {
