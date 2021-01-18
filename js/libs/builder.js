604530f2ea53b5e1684b7aa72cc43d58d28322cf
AndrRyaz <andriy.r@upplabs.com>
2021-01-18 16:25:07 +0200

    Fix to checkbox default value
    Fix
    
    revert builder

--- js/libs/builder.js
+++ js/libs/builder.js
@@ -451,7 +451,7 @@ new Vue({
             Fliplet.Studio.emit('reload-widget-instance', Fliplet.Widget.getDefaultId());
           });
           Fliplet.Widget.autosize();
-          Fliplet.Widget.setSaveButtonLabel(SAVE_BUTTON_LABELS.SAVE_AND_CLOSE);
+          Fliplet.Widget.setSaveButtonLabel(SAVE_BUTTON_LABELS.SAVE);
         });
       });
     },
@@ -482,7 +482,7 @@ new Vue({
           Fliplet.Studio.emit('reload-widget-instance', Fliplet.Widget.getDefaultId());
         });
         Fliplet.Widget.autosize();
-        Fliplet.Widget.setSaveButtonLabel(SAVE_BUTTON_LABELS.SAVE_AND_CLOSE);
+        Fliplet.Widget.setSaveButtonLabel(SAVE_BUTTON_LABELS.SAVE);
       });
     },
     checkEmailTemplate: function() {
