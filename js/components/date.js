bace6b86fcfca4570609d25c1d582c6c6d83ee11
AndrRyaz <andriy.r@upplabs.com>
2021-03-29 13:43:34 +0300

    fix to datePicker value

--- js/components/date.js
+++ js/components/date.js
@@ -45,6 +45,16 @@ Fliplet.FormBuilder.field('date', {
       return Fliplet.Env.get('platform') === 'web';
     }
   },
+  methods: {
+    updateValue: function(value) {
+      if (value) {
+        this.value = value;
+      }
+
+      this.highlightError();
+      this.$emit('_input', this.name, this.value);
+    }
+  },
   mounted: function() {
     var $vm = this;
 
@@ -81,5 +91,12 @@ Fliplet.FormBuilder.field('date', {
 
     this.$emit('_input', this.name, this.value);
     $vm.$v.$reset();
+  },
+  watch: {
+    value: function(val) {
+      if (!val) {
+        this.updateValue(moment().format(DATE_FORMAT));
+      }
+    }
   }
 });
