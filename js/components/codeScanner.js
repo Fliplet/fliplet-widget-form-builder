22e9ab32af0e886e043c682e409cad1685f50d0c
Armine Simonyan <128052346+armine-fliplet@users.noreply.github.com>
2023-09-05 12:09:23 +0400

    ID-3516 [FEAT] Implement code scanner form field (#573)
    * ID-3516 [FEAT] Implement code scanner form field
    
    * ID-3517 [FEAT] Implement code scanner UI
    
    * ID-3517 [FEAT] Implemented scanner textarea UI
    
    * ID-3518 [FEAT] Set value when scanned text exists
    
    * Small fixes
    
    * Parsed error message

---
+++ js/components/codeScanner.js
@@ -0,0 +1,68 @@
+Fliplet.FormBuilder.field('codeScanner', {
+  name: 'Code scanner',
+  category: 'Advanced',
+  props: {
+    value: {
+      type: String
+    },
+    description: {
+      type: String
+    },
+    placeholder: {
+      type: String
+    }
+  },
+  data: function() {
+    return {
+      errorMessage: null
+    };
+  },
+  validations: function() {
+    var rules = {
+      value: {}
+    };
+
+    if (this.required && !this.readonly) {
+      rules.value.required = window.validators.required;
+    }
+
+    return rules;
+  },
+  created: function() {
+    Fliplet.FormBuilder.on('reset', this.onReset);
+  },
+  destroyed: function() {
+    Fliplet.FormBuilder.off('reset', this.onReset);
+  },
+  methods: {
+    openScanner: function() {
+      var $vm = this;
+
+      if (this.readonly) {
+        return;
+      }
+
+      Fliplet.Barcode.scan().then(function(result) {
+        if (result.text) {
+          $vm.value = result.text;
+        }
+      })
+        .catch(function(error) {
+          $vm.errorMessage = Fliplet.parseError(error);
+        });
+    },
+    onReset: function() {
+      this.errorMessage = null;
+    },
+    scannerInput: function(event) {
+      var el = event.target;
+      var maxRows = 5;
+
+      el.rows = 1;
+
+      while (el.scrollHeight > el.clientHeight && el.rows < maxRows) {
+        el.rows++;
+      }
+    }
+  }
+});
