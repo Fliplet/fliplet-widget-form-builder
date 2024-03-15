191a78f3989e16f6a7518817f7789cf1a9e70ee5
Armine Simonyan <128052346+armine-fliplet@users.noreply.github.com>
2024-03-15 15:59:42 +0400

    ID-3982 [FIX] Ensure mobile supports all the plugins (#605)

--- js/components/wysiwyg.js
+++ js/components/wysiwyg.js
@@ -80,7 +80,11 @@ Fliplet.FormBuilder.field('wysiwyg', {
     var config = {
       target: this.$refs.textarea,
       mobile: {
-        toolbar_mode: 'floating'
+        toolbar_mode: 'floating',
+        plugins: [
+          'advlist', 'autolink', 'lists', 'link', 'directionality',
+          'autoresize', 'fullscreen', 'code', 'paste', 'wordcount', 'table'
+        ]
       },
       readonly: this.readonly,
       placeholder: this.placeholder,
